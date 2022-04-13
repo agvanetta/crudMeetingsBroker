b1b68a1cdfd09965ad2d61e72811d7cc0fddbb13
Agustin Vanetta <agvanetta@gmail.com>
2022-04-12 23:58:24 -0300

    queda agregar condicionales

--- Back _Java_CRUD/src/main/java/com/example1/trabajoIntegrador/controller/AsesorController.java
+++ Back _Java_CRUD/src/main/java/com/example1/trabajoIntegrador/controller/AsesorController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/asesores")
 public class AsesorController {
 
